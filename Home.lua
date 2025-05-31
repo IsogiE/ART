2181f0a0f47653816bd4590ffc0f19d4676b8c68
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-06-01 01:11:43 +0200

    o7

--- Home.lua
+++ Home.lua
@@ -28,6 +28,21 @@ function HomeModule:CreateConfigPanel(parent)
     logo:SetTexture("Interface\\AddOns\\ACT\\media\\logo.tga")
     logo:SetTexCoord(0.05, 0.95, 0.05, 0.95)
 
+    local brick = configPanel:CreateTexture(nil, "ARTWORK")
+    brick:SetSize(64, 64)
+    brick:SetPoint("TOP", configPanel, "TOP", -120, -450)
+    brick:SetTexture("Interface\\AddOns\\ACT\\media\\roiben.tga")
+    brick:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+
+    local cope = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    cope:SetPoint("BOTTOM", brick, "TOP", 0, 12)
+    cope:SetText("o7 won MDI in our hearts")
+
+    local o7 = configPanel:CreateTexture(nil, "ARTWORK")
+    o7:SetSize(24, 24)
+    o7:SetPoint("RIGHT", cope, "LEFT", -6, 0) 
+    o7:SetTexture("Interface\\AddOns\\ACT\\media\\o7.png")
+
     local info = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     info:SetPoint("TOP", logo, "BOTTOM", 0, -10)
     info:SetText("|cff00ccffAdvance Custom Tools|r\n\n|cffffcc00The #1 way to upset your raiders|r")
