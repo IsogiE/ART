f0cee654dd1819dc17a4a68303e22f4beb89246b
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-06-20 16:34:42 +0200

    Start to add build versions to prep for retail release

--- Assignments/Assignment.lua
+++ Assignments/Assignment.lua
@@ -1,5 +1,9 @@
-AssignmentData = {
-    {
+local _, _, _, buildVersion = GetBuildInfo()
+
+AssignmentData = {}
+
+if buildVersion >= 110100 then
+    table.insert(AssignmentData, {
         id = "liberationofundermine",
         name = "Liberation of Undermine",
         bosses = {
@@ -10,8 +14,11 @@ AssignmentData = {
             { id = "mugzee",         name = "Mug'Zee" },
             { id = "gallywix",       name = "Gallywix" },
         }
-    },
-    {
+    })
+end
+
+if buildVersion >= 110200 then
+    table.insert(AssignmentData, {
         id = "manaforgeomega",
         name = "Manaforge Omega",
         bosses = {
@@ -24,5 +31,5 @@ AssignmentData = {
             { id = "salhadaar",     name = "Nexus-King" },
             { id = "dimensius",     name = "Dimensius" },
         }
-    }
-}
+    })
+end
