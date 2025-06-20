33db0d11539d6abbe52d57cbcd03cdf18c4f5187
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-06-20 14:56:13 +0200

    Adding MFO + all bosses for now

--- Assignments/Assignment.lua
+++ Assignments/Assignment.lua
@@ -8,7 +8,21 @@ AssignmentData = {
             { id = "sprocketmonger", name = "Sprocketmonger" },
             { id = "onearmedbandit", name = "One-Armed Bandit" },
             { id = "mugzee",         name = "Mug'Zee" },
-            { id = "gallywix",     name = "Gallywix" }
+            { id = "gallywix",       name = "Gallywix" },
+        }
+    },
+    {
+        id = "manaforgeomega",
+        name = "Manaforge Omega",
+        bosses = {
+            { id = "plexus",        name = "Plexus Sentinel" },
+            { id = "loomithar",     name = "Loom'ithar" },
+            { id = "naazindhri",    name = "Naazindhri" },
+            { id = "araz",          name = "Forgeweaver" },
+            { id = "soulhunters",   name = "Soul Hunters" },
+            { id = "fractillus",    name = "Fractillus" },
+            { id = "salhadaar",     name = "Nexus-King" },
+            { id = "dimensius",     name = "Dimensius" },
         }
     }
 }
