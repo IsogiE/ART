5682b417519ad8bff338077ae425e089745f4f71
IsogiE <88090493+IsogiE@users.noreply.github.com>
2025-08-30 23:01:43 +0200

    hatewatch

--- Home.lua
+++ Home.lua
@@ -28,25 +28,25 @@ function HomeModule:CreateConfigPanel(parent)
     logo:SetTexture("Interface\\AddOns\\ACT\\media\\logo.tga")
     logo:SetTexCoord(0.05, 0.95, 0.05, 0.95)
 
-    local brick = configPanel:CreateTexture(nil, "ARTWORK")
-    brick:SetSize(64, 64)
-    brick:SetPoint("TOP", configPanel, "TOP", -120, -450)
-    brick:SetTexture("Interface\\AddOns\\ACT\\media\\roiben.tga")
-    brick:SetTexCoord(0.05, 0.95, 0.05, 0.95)
-
-    local cope = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    cope:SetPoint("BOTTOM", brick, "TOP", 0, 12)
-    cope:SetText("o7 won MDI in our hearts")
-
-    local o7 = configPanel:CreateTexture(nil, "ARTWORK")
-    o7:SetSize(24, 24)
-    o7:SetPoint("RIGHT", cope, "LEFT", -6, 0)
-    o7:SetTexture("Interface\\AddOns\\ACT\\media\\o7.tga")
-
     local info = configPanel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     info:SetPoint("TOP", logo, "BOTTOM", 0, -10)
     info:SetText("|cff00ccffAdvance Custom Tools|r\n\n|cffffcc00The #1 way to upset your raiders|r")
     info:SetJustifyH("CENTER")
+
+    local hatewatch = configPanel:CreateTexture(nil, "ARTWORK")
+    hatewatch:SetSize(600, 500)
+    hatewatch:SetPoint("CENTER", configPanel, "CENTER", -120, 50)
+    hatewatch:SetTexture("Interface\\AddOns\\ACT\\media\\hatewatch.tga")
+
+    local brickleft = configPanel:CreateTexture(nil, "ARTWORK")
+    brickleft:SetSize(32, 32)
+    brickleft:SetPoint("TOP", configPanel, "CENTER", -260, -170)
+    brickleft:SetTexture("Interface\\AddOns\\ACT\\media\\roiben.tga")
+
+    local brickright = configPanel:CreateTexture(nil, "ARTWORK")
+    brickright:SetSize(32, 32)
+    brickright:SetPoint("TOP", configPanel, "CENTER", 20, -170)
+    brickright:SetTexture("Interface\\AddOns\\ACT\\media\\roiben.tga")
 
     self.configPanel = configPanel
     return configPanel
