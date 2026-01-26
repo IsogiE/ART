f6a69725692846756cd1c43cb225de80b17bcd91
IsogiE <88090493+IsogiE@users.noreply.github.com>
2026-01-26 19:00:07 +0100

    Adding Xeph's quest cleanup fix
    https://github.com/ljosberinn/XephFix/blob/main/Modules/HiddenQuestCleanup.lua

--- Core.lua
+++ Core.lua
@@ -310,3 +310,36 @@ function ACT:OnEnable()
 
     self:PreloadModuleUIs()
 end
+
+-- Putting this here for now till/if I ever find anywhere better to put it. 
+-- All credits to XephFix (pulled from his GitHub, he's the goat I didn't write this, guild just doesn't want yet another addon to install)
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", function()
+	local numShownEntries, numQuests = C_QuestLog.GetNumQuestLogEntries()
+
+	if numShownEntries <= numQuests then
+		return
+	end
+
+	local total = 0
+
+	for i = 1, C_QuestLog.GetNumQuestLogEntries() do
+		local quest = C_QuestLog.GetInfo(i)
+
+		if quest and quest.isHidden then
+			local wasRemoved = C_QuestLog.RemoveQuestWatch(i)
+
+			if wasRemoved then
+				--print(string.format("unwatched quest %s (%d)", quest.title, quest.questID))
+				total = total + 1
+			else
+				--print(string.format("could not unwatch quest %s (%d)", quest.title, quest.questID))
+			end
+		end
+	end
+
+	if total > 0 then
+		--print(string.format("unwatched %d quests", total))
+	end
+end)
